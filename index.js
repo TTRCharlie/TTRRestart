5cb07b2a46537c7716607fd74d1c0604f1ab207a
Zahorecz Balázs <admin@ttr3.tk>
2021-03-01 13:15:19 +0100

    Restart with sleep, and announce

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const Pterodactyl = require('pterodactyl.js');
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
  
 const client = new Pterodactyl.Builder()
-    .setURL('Pterodactyl panel URL')
-    .setAPIKey('API Key')
+    .setURL('https://creative.ttr3.tk')
+    .setAPIKey('av8tWUa9fIaMVKdfIn6BNFrl6IRmJ8hd2utGrNfyj0KbMGuv')
     .asUser();
  
 client.getClientServers()
 .then(async servers => {
     let server = servers[4];
  
+    await server.sendCommand("bc Server will restart in 10 seconds")
+    await sleep(10000)
     await server.restart();
+    
  
 }).catch(error => console.log(error));
