61f1989554a2c8cbd7a71135852376c43a56c8a6
Charlie <info@ttr3.eu>
2021-03-01 13:50:39 +0100

    Delet sleep()

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Pterodactyl = require('pterodactyl.js');
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+const Pterodactyl = require('pterodactyl.js');
  
 const client = new Pterodactyl.Builder()
     .setURL('https://creative.ttr3.tk')
@@ -12,9 +12,7 @@ client.getClientServers()
 .then(async servers => {
     let server = servers[4];
  
-    await server.sendCommand('bc Server will restart in 10 seconds');
-    sleep(10000);
-    await server.restart();
-    
+    await server.sendCommand("bc Server will restart in 10 seconds")
+    await server.restart()
  
 }).catch(error => console.log(error));
