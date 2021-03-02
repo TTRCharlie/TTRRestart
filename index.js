8edafaf4f193956f5cad18a69427272783e5d04e
Charlie <info@ttr3.eu>
2021-03-02 09:35:16 +0100

    Dublicated getClientServers function"

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const Pterodactyl = require('pterodactyl.js');
  
 const client = new Pterodactyl.Builder()
-    .setURL('Pterodactyl panel URL')
-    .setAPIKey('Yout API Key')
+    .setURL('https://creative.ttr3.tk')
+    .setAPIKey('xMORJrUOZGtnbnquNz5sPM9UTFMZMqz2zFpXG8pjSi7fHoQa')
     .asUser();
  
 client.getClientServers()
 .then(async servers => {
     let server = servers[4];
  
-    await server.sendCommand("bc Server will restart in 10 seconds")
+    await server.sendCommand('bc Server will restart in 10 seconds');
+    
+});
+
+client.getClientServers()
+.then(async servers => {
+    let server = servers[4];
+ 
     await setTimeout(() => {  server.restart(); }, 10000);
     
 }).catch(error => console.log(error));
